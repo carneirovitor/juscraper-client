64bb0ed578cc290ac4df6007877c058d7985d419
carneirovitor <viitor.augusto@gmail.com>
2020-01-31 16:18:09 -0300

    Update index.js

--- sigjur/index.js
+++ sigjur/index.js
@@ -1,23 +1,3 @@
-/* var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/jbscraper');
-
-var lawsuitSchema = new mongoose.Schema({
-    lsnumber: String,
-    lsresume: String
-}, { collection: 'lawsuits' }
-);
-
-router.get('/lawsuits', function (req, res, next) {
-    var db = require('../lawsuits/db');
-    var Lawsuit = db.Mongoose.model('lawsuits', db.LawsuitSchema, 'lawsuits');
-    Lawsuit.find({}).lean().exec(function(e,docs){
-       res.json(docs);
-       res.end();
-    });
-});
-
-module.exports = { Mongoose: mongoose, LawsuitSchema: lawsuitSchema } */
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -27,12 +7,13 @@ require('./models/Lawsuits');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(express.static('client/src'));
 
 //IMPORT ROUTES
 require('./routes/lawsuitRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/src'));
+    
 
   const path = require('path');
   app.get('*', (req,res) => {
