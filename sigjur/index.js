18a2d8604cd37bb22bfa0e8afd8b52e2ba94da53
carneirovitor <viitor.augusto@gmail.com>
2020-01-31 17:52:56 -0300

    Update index.js

--- sigjur/index.js
+++ sigjur/index.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'production') {
 
 
   app.get('*', (req,res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'src', 'index.html'))
+      res.sendFile(path.resolve(__dirname, 'client', 'src', 'index.js'))
   })
 
 }
