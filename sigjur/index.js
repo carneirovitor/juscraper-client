d853ea02414fb4e8da34ab28081038586d523dc2
carneirovitor <viitor.augusto@gmail.com>
2020-01-31 20:41:31 -0300

    Update index.js

--- sigjur/index.js
+++ sigjur/index.js
@@ -13,13 +13,11 @@ require('./routes/lawsuitRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   console.log(path.join(__dirname, 'client/build'))
-  app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname,'client', 'public')));
 
-  const path = require('path');
-  app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-    console.log(path.join(__dirname+'/client/build/index.html'));
-  });
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname,'client', 'public', 'index.html'));
+});
 
 }
 
