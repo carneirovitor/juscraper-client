fa5b08580a336b64bcd38b48a62a941258b674b7
carneirovitor <viitor.augusto@gmail.com>
2020-01-31 18:00:08 -0300

    Update index.js

--- sigjur/index.js
+++ sigjur/index.js
@@ -6,18 +6,18 @@ const path = require('path');
 require('./models/Lawsuits');
 
 const app = express();
-app.use(express.static(__dirname + '/client/src'));
+app.use(express.static(__dirname + '/client/src/lawsuit-search'));
 app.use(bodyParser.json());
 
 //IMPORT ROUTES
 require('./routes/lawsuitRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(__dirname + '/client/src'));
+    app.use(express.static(__dirname + '/client/src/lawsuit-search'));
 
 
   app.get('*', (req,res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'src', 'index.js'))
+      res.sendFile(path.resolve(__dirname, 'client', 'src','lawsuit-search', 'LawsuitSearch.js'))
   })
 
 }
