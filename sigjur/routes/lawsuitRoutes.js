f582ab5ad7f57dc53fb46a02bd77f9f955cfe3fc
carneirovitor <viitor.augusto@gmail.com>
2020-01-31 16:09:40 -0300

    Update lawsuitRoutes.js

--- sigjur/routes/lawsuitRoutes.js
+++ sigjur/routes/lawsuitRoutes.js
@@ -1,6 +1,5 @@
 // /routes/productRoutes.js
 const mongoose = require('mongoose');
-const Lawsuit = mongoose.model('lawsuits');
 const LawsuitScraper = require('../lawsuits/jbscraper')
 const path = require('path');
 
@@ -17,38 +16,5 @@ module.exports = async (app) => {
     const lawsuit = await LawsuitScraper.callScraper(req.query.lawsuitNumber)
     return res.send(lawsuit);
   });
-
-  app.post(`/api/lawsuits`, async (req, res) => {
-    let lawsuit = await Lawsuit.create(req.body);
-    return res.status(201).send({
-      error: false,
-      lawsuit
-    })
-  })
-
-
-  app.put(`/api/lawsuits/:id`, async (req, res) => {
-    const {id} = req.params;
-
-    let lawsuit = await Lawsuit.findByIdAndUpdate(id, req.body);
-
-    return res.status(202).send({
-      error: false,
-      lawsuit
-    })
-
-  });
-
-  app.delete(`/api/lawsuit/:id`, async (req, res) => {
-    const {id} = req.params;
-
-    let lawsuit = await Lawsuit.findByIdAndDelete(id);
-
-    return res.status(202).send({
-      error: false,
-      lawsuit
-    })
-
-  })
 
 }
