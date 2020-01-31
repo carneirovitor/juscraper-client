c8199d7eed2947f5ee54b578302c865c7275a0ad
carneirovitor <viitor.augusto@gmail.com>
2020-01-31 18:58:54 -0300

    Add files via upload

---
+++ sigjur/client/build/precache-manifest.70e92d5f1178c0a7d4e1d7c61dc4fe9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "716935dcceaf1e9690ecb50a6d82dd0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f280c8e76df85c92bd19",
+    "url": "/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "006174d1958afb1803f9",
+    "url": "/static/css/main.4c87af94.chunk.css"
+  },
+  {
+    "revision": "f280c8e76df85c92bd19",
+    "url": "/static/js/2.ed871d5f.chunk.js"
+  },
+  {
+    "revision": "feb849e954c25333524ec5b5e4aff1c0",
+    "url": "/static/js/2.ed871d5f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "006174d1958afb1803f9",
+    "url": "/static/js/main.03046adf.chunk.js"
+  },
+  {
+    "revision": "0281e3aa3ea9fc02c242",
+    "url": "/static/js/runtime-main.b12ff581.js"
+  }
+]);
